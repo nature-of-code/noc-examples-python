37b213707ed666fe37b61301f86fab41f060272a
Alexandre B A Villares <abav@lugaralgum.com>
2018-06-04 19:54:18 -0300

    7_01 Wolfram

---
+++ chp07_CA/Exercise_7_01_WolframCA_randomizedrules/CA.py
@@ -0,0 +1,94 @@
+# The Nature of Code
+# Daniel Shiffman
+# http://natureofcode.com
+
+# Wolfram Cellular Automata
+
+# A class to manage the CA
+
+class CA():
+
+    w = 5
+
+    def __init__(self, r):
+        # A list to store the ruleset, for example:0,1,1,0,1,1,0,1
+        self.ruleset = r
+        # A list of 0s and 1s
+        self.cells = [0] * (width / CA.w)
+        self.restart()
+
+    # Make a random ruleset
+    def randomize(self):
+        for i in range(8):
+            self.ruleset[i] = int(random(2))
+
+    # Reset to generation 0
+    def restart(self):
+        for i in range(len(self.cells)):
+            self.cells[i] = 0
+
+        # We arbitrarily start with just the middle cell having a state of "1"
+        self.cells[len(self.cells) / 2] = 1
+        self.generation = 0
+
+    # The process of creating the new generation
+    def generate(self):
+        # First we create an empty array for the new values
+        nextgen = [0] * len(self.cells)
+        # For every spot, determine new state by examing current state, and neighbor states
+        # Ignore edges that only have one neighor
+        for i in range(1, len(self.cells) - 1):
+            left = self.cells[i - 1]   # Left neighbor state
+            me = self.cells[i]       # Current state
+            right = self.cells[i + 1]   # Right neighbor state
+            # Compute next generation state based on ruleset
+            nextgen[i] = self.rules(left, me, right)
+
+        # The current generation is the new generation
+        self.cells = nextgen
+        self.generation += 1
+
+    # This is the easy part, just draw the cells, fill 255 for '1', fill 0 for
+    # '0'
+    def display(self):
+        for i, cell in enumerate(self.cells):
+            if cell == 1:
+                fill(0)
+            else:
+                fill(255)
+            noStroke()
+            rect(i * CA.w, self.generation * CA.w, CA.w, CA.w)
+
+    # Implementing the Wolfram rules
+    # This is the concise conversion to binary way
+    # def rules(self, a, b, c):
+    #     s = str(a) + str(b) + str(c)
+    #     index = int(s, 2)
+    #     return self.ruleset[index]
+    
+    def rules(self, a, b, c):
+        ruleset = self.ruleset
+        if a == 1 and b == 1 and c == 1:
+            return ruleset[0]
+        if a == 1 and b == 1 and c == 0:
+            return ruleset[1]
+        if a == 1 and b == 0 and c == 1:
+            return ruleset[2]
+        if a == 1 and b == 0 and c == 0:
+            return ruleset[3]
+        if a == 0 and b == 1 and c == 1:
+            return ruleset[4]
+        if a == 0 and b == 1 and c == 0:
+            return ruleset[5]
+        if a == 0 and b == 0 and c == 1:
+            return ruleset[6]
+        if a == 0 and b == 0 and c == 0:
+            return ruleset[7]
+        return 0
+
+    # The CA is done if it reaches the bottom of the screen
+    def finished(self):
+        if self.generation > height / CA.w:
+            return True
+        else:
+            return False
---
+++ chp07_CA/Exercise_7_01_WolframCA_randomizedrules/Exercise_7_01_WolframCA_randomizedrules.pyde
@@ -0,0 +1,43 @@
+# The Nature of Code
+# Daniel Shiffman
+# http://natureofcode.com
+
+# Wolfram Cellular Automata
+
+# Simple demonstration of a Wolfram 1-dimensional cellular automata
+# When the system reaches bottom of the window, it restarts with a new ruleset
+# Mouse click restarts as well
+
+# import CA a class to describe a Wolfram elementary Cellular Automata
+from CA import *
+
+delay = 0
+
+def setup():
+    global ca
+    size(800, 200)
+    background(255)
+    ruleset = [0, 1, 0, 1, 1, 0, 1, 0]  # An initial rule system
+    ca = CA(ruleset)                    # Initialize CA
+    frameRate(30)
+
+
+def draw():
+    global delay
+    ca.display()          # Draw the CA
+    ca.generate()
+
+    # If we're done, clear the screen, pick a new ruleset and restart
+    if ca.finished():
+        delay += 1
+        if delay > 30:
+            background(255)
+            ca.randomize()
+            ca.restart()
+            delay = 0
+
+
+def mousePressed():
+    background(255)
+    ca.randomize()
+    ca.restart()
